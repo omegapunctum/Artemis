eecb65508284a2e916015e548ddd525f18f90f74
omegapunctum <267074110+omegapunctum@users.noreply.github.com>
2026-03-16 21:19:33 +0500

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,12 @@
+// ── СОСТОЯНИЕ ─────────────────────────────────────────────
+const state = {
+  year:      1200,
+  from:      1000,
+  to:        1400,
+  range:     false,
+  query:     '',
+  active:    new Set(['romanesque', 'gothic', 'renaissance']),
+  showZones: true,
+  features:  [],
+  markers:   [],
+};
